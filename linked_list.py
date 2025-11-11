bf8266fab49d9faad6cff6f2aa06376befb475a6
Harsh-verma0426 <lionworld13913@gmail.com>
2025-11-11 07:25:27 +0530

    Added Linked list data structure

---
+++ linked_list.py
@@ -0,0 +1,78 @@
+from Data_structure_in_python.node import Node
+
+class LinkedList:
+    def __init__(self,size=0):
+        self.head = None
+        self.tail = None
+        self.size = size
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.size += 1
+
+    def display(self):
+        current = self.head
+        elements = []
+        while current:
+            elements.append(current.value)
+            current = current.next
+        print(elements)
+
+    def get_size(self):
+        return self.size
+    
+    def search(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+    
+    def delete(self, value):
+        if self.head is None:
+            return False
+
+        if self.head.value == value:
+            self.head = self.head.next
+            self.size -= 1
+            if self.head is None:
+                self.tail = None
+            return True
+
+        current = self.head
+        while current.next:
+            if current.next.value == value:
+                current.next = current.next.next
+                if current.next is None:
+                    self.tail = current
+                self.size -= 1
+                return True
+            current = current.next
+
+        return False
+
+    def insert(self, index, value):
+
+            if index == 0:
+                self.prepend(value)
+            
+            elif index == self.size:
+                self.append(value)
+                
